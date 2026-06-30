3e9dabecf1be7b1a5c66b06fa6cbd8a215ed727a
ar-sohan <arsohan101@gmail.com>
2026-06-30 19:23:16 +0600

    two routes created

---
+++ src/admin/admin.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { AdminService } from './admin.service';
+
+@Controller()
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+
+  @Get()
+  getHi(): string {
+    return this.adminService.getHi();
+  }
+}
